40ab5ca37f94d29d089117c2cd3fdf3d20e3426e
신성민 <cydiazero@gmail.com>
2022-11-13 20:23:08 +0900

    VZ32 plain 아키텍쳐 업로드
    Plain 아키텍쳐는 파이프라이닝도 없고 아무것도 없는 그냥 VZ32 구현집합체입니다.
    군머라 폰으로 코드를 짜야되서 폴더구분이 없..

---
+++ VZ32Plain.v
@@ -0,0 +1,23 @@
+module vz32_decoder()
+
+endmodule
+
+module regfile_32x32()
+
+endmodule
+
+module regfile_16x64()
+
+endmodule
+
+module alu()
+
+endmodule
+
+module fp()
+
+endmodule
+
+module vz32_plain()
+
+endmodule
