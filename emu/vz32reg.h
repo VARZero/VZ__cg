e0604c9240a9936d34fc1f06532740b50f0ccc3d
VARZero <cydiazero@gmail.com>
2022-11-13 23:44:42 +0900

    폴더 정리 및 xlsx 수정, 및 plain rtl 작성 시작

---
+++ emu/vz32reg.h
@@ -0,0 +1,10 @@
+unsigned int intreg[31];
+
+enum iregs{
+    Zero, Back, Bp, Sp,
+    i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,
+    i11,i12,i13,i14,i15,i16,i17,i18,i19,i20,
+    ItrrN, ItrrT, ItrrD1, ItrrD2,
+    KrnR1, KrnR2, KrnR3, KrnR4
+};
+
