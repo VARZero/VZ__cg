3839a0cebb9bceb56188b274d12fdd676bd49527
신성민 <cydiazero@gmail.com>
2023-03-19 17:28:04 +0900

    라인 에러 체크 기능까지 완성

--- tools/binutils-vz32-plain/as.c
+++ tools/binutils-vz32-plain/as.c
@@ -11,7 +11,7 @@ char get_Register_out_Bin(char* RegName, char* error){
             if (strcmp(RegName, vz32_name_normal_opcode[regc]) == 0){return regc;}
             if (strcmp(RegName, vz32_name_functional_opcode[regc]) == 0){return regc;}
         }
-        error = "Invalid Regster name.";
+        strcpy(error, "Invalid Regster name.");
         return 0xFF;
 }
 
@@ -31,13 +31,14 @@ bool command_syntax(char* line, uint32_t* outputBin, char* error){
             }
         }
         if (error[0] == 'P'){
-            error = strcat(op, " is not vz32 opcode.\0");
+            strcpy(error, op);
+            strcat(error, " is not vz32 opcode.\0");
             return 0;
         }
 
     // Opcode에 맞는 타입인가?
         char Rn[3][10];
-        char IMM[50];
+        char IMM[50], *tIMM;
         for(short RegCnt = 0; RegCnt < nowOp->type; RegCnt++){
             strcpy(Rn[RegCnt], strtok(NULL, " ,"));
             if (Rn[RegCnt] == NULL){
@@ -47,8 +48,11 @@ bool command_syntax(char* line, uint32_t* outputBin, char* error){
             *outputBin = *outputBin | (get_Register_out_Bin(Rn[RegCnt], error) << ( 7 + RegCnt*5 ));
             if (error[0] == 'I'){return 0;}
         }
-        strcpy(IMM, strtok(NULL, " ,"));
-        *outputBin = *outputBin | ( atoi(IMM) << ( 7 + nowOp->type*5 ));
+        tIMM = strtok(NULL, " ,");
+        if (tIMM != NULL){
+            strcpy(IMM, tIMM);
+            *outputBin = *outputBin | ( atoi(IMM) << ( 7 + nowOp->type*5 ));
+        }
 
         error = " ";
         if (strtok(NULL, " ,") != NULL){
@@ -61,16 +65,16 @@ bool command_syntax(char* line, uint32_t* outputBin, char* error){
 
 int main(/*int argc, char* argv*/){
     //open();
-    char line[2][50] = {"ADD-IMM tm1, tm2, 3", "SUB i0, i2, i14"};
-    uint32_t outBin[2] = {0, 0};
+    char line[4][50] = {"ADD-IMM tm1, tm2, 3", "EAA i0, i1", "SUB i0, i2, i14", "ADD i99, i92, e32"};
+    uint32_t outBin[4] = {0, 0};
     char error[50];
-    for (char i = 0; i < 2; i++){
+    for (char i = 0; i < 4; i++){
         if (command_syntax(line[i], &outBin[i], error) != 1){
             printf("%s\n", error);
         }
     }
-    for (char i = 0; i < 2; i++){
-        printf("%s %X\n", line[2], outBin[2]);
+    for (char i = 0; i < 4; i++){
+        printf("%X\n", outBin[i]);
     }
     return 0;
 }
