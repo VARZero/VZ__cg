530e3afa304d48ef9cff82cc7b7b0aa686fd14a2
신성민 <cydiazero@gmail.com>
2023-03-19 14:53:00 +0900

    command_syntax 작성완료

--- tools/binutils-vz32-plain/as.c
+++ tools/binutils-vz32-plain/as.c
@@ -4,27 +4,38 @@
 
 bool command_syntax(char* line, char* outputBin, char* error){
     // Opcode 확인 및 추가
-        char op[30] = strtok(line, " ,");
+        char op[30] = strtok(line, " ");
         vz32_opcode* nowOp;
         error[0] = "P";
         for(short OpIdx = 0; OpIdx < vz32_opcode_count; OpIdx++){
             if (strcmp(op, vz32_list_opcodes[OpIdx]) == 0){
                 nowOp = &vz32_list_opcodes[OpIdx];
                 outputBin = vz32_list_opcodes[OpIdx].B_opc & 0b11111 
-                    + vz32_list_opcodes[OpIdx].B_func & 0b11;
+                    + ((vz32_list_opcodes[OpIdx].B_func & 0b11) << 5);
                 error[0] = "C"; break;
             }
         }
         if (error[0] == "P"){
-            error = strcat(op, " is not vz32 opcode.");
+            error = strcat(op, " is not vz32 opcode.\0");
             return 0;
         }
 
     // Opcode에 맞는 타입인가?
-        for(short RegCnt = 0; RegCnt <= nowOp.type; RegCnt){
-            // 작성중
+        char IMM[50] = strtok(NULL, " ,");
+        char Rn[3][10];
+        for(short RegCnt = 0; RegCnt < nowOp.type; RegCnt++){
+            Rn[RegCnt] = strtok(NULL, " ,");
+            if (Rn[RegCnt] == NULL){
+                error = "Lack Argument.\0";
+                return 0;
+            }
         }
-
+        error = " ";
+        if (strtok(NULL, " ,") != NULL){
+            // 추가적으로 입력된 레지스터가 있어서 경고
+            error = "Too many argument.\0";
+        }
+    
     return 1;
 }
 
